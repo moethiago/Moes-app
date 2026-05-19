0eba4399bbcd7cce08a5bc5ee61dc9c10346c89c
moethiago <mo3ath306@gmail.com>
2026-05-19 17:43:31 +0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,23 @@
+function switchTab(tab){
+
+  document.querySelectorAll(".tab-panel")
+
+    .forEach(p=>p.classList.remove("active"));
+
+  document.getElementById("panel-"+tab).classList.add("active");
+
+}
+
+function init(){
+
+  loadStorage();
+
+  loadNewsFeed();
+
+  loadF1();
+
+  renderHealth();
+
+}
+
+document.addEventListener("DOMContentLoaded", init);
