c11ccf270c5d5613aa0fb494c9f1abe4f0938e08
moethiago <mo3ath306@gmail.com>
2026-05-19 17:38:49 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,17 @@
+const NEXT_RACE = {
+
+  title: "Canadian Grand Prix",
+
+  location: "Circuit Gilles Villeneuve, Montreal",
+
+  date: "2026-05-24T20:00:00Z"
+
+};
+
+const TARGETS = { kcal:2500, prot:180, carb:280, fat:70 };
+
+const F1_DATA = [/* your full driver list */];
+
+const TEAM_COLORS = { /* unchanged */ };
+
+const FALLBACK_NEWS = [/* your news array */];
