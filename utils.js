855322e735a055e94a45bf495cb4215401434a23
moethiago <mo3ath306@gmail.com>
2026-05-19 18:28:08 +0300

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,21 +1,5 @@
-function timeAgo(val){
+// utils.js
 
-  if(typeof val === "string") return val;
+export function $(id){return document.getElementById(id);}
 
-  const diff = (Date.now()-val)/1000;
-
-  if(diff<60) return "just now";
-
-  if(diff<3600) return Math.floor(diff/60)+"m ago";
-
-  if(diff<86400) return Math.floor(diff/3600)+"h ago";
-
-  return Math.floor(diff/86400)+"d ago";
-
-}
-
-function openLink(url){
-
-  if(url && url !== "#") window.open(url,"_blank");
-
-}
+export function time(){return new Date().toLocaleTimeString().slice(0,5);}
