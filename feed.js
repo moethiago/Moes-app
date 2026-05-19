80589e192400f15e8ea452767a77a65ba0679956
moethiago <mo3ath306@gmail.com>
2026-05-19 18:29:01 +0300

    Update feed.js

--- feed.js
+++ feed.js
@@ -1,85 +1,17 @@
-let parsedStoriesCache = [];
+// feed.js
 
-let currentFilter = "ALL";
+export function renderFeed(container){
 
-async function loadNewsFeed(){
+  const data = [
 
-  parsedStoriesCache = [];
+    "F1 season heating up",
 
-  const channels = [
+    "Football weekend results",
 
-    {name:"BBC F1", url:"https://feeds.bbci.co.uk/sport/formula1/rss.xml", cat:"F1"},
-
-    {name:"BBC Football", url:"https://feeds.bbci.co.uk/sport/football/rss.xml", cat:"FOOTBALL"}
+    "Saudi sports updates"
 
   ];
 
-  for(const ch of channels){
-
-    try{
-
-      const res = await fetch("https://api.rss2json.com/v1/api.json?rss_url="+encodeURIComponent(ch.url));
-
-      const data = await res.json();
-
-      if(data.items){
-
-        data.items.forEach(i=>{
-
-          parsedStoriesCache.push({
-
-            title:i.title,
-
-            link:i.link,
-
-            cat:ch.cat,
-
-            src:ch.name,
-
-            time:Date.parse(i.pubDate)
-
-          });
-
-        });
-
-      }
-
-    }catch(e){}
-
-  }
-
-  renderNews();
-
-}
-
-function renderNews(){
-
-  const panel = document.getElementById("panel-feed");
-
-  const list = currentFilter==="ALL"
-
-    ? parsedStoriesCache
-
-    : parsedStoriesCache.filter(x=>x.cat===currentFilter);
-
-  panel.innerHTML = list.map(n=>`
-
-    <div class="wire-item" onclick="openLink('${n.link}')">
-
-      <div class="wire-headline">${n.title}</div>
-
-      <div class="wire-meta">${n.src}</div>
-
-    </div>
-
-  `).join("");
-
-}
-
-function setFeedFilter(cat){
-
-  currentFilter = cat;
-
-  renderNews();
+  container.innerHTML = data.map(d=>`<div class="card">${d}</div>`).join("");
 
 }
