69547ae2fab117e99fbc956b73035a0826820ea5
moethiago <mo3ath306@gmail.com>
2026-06-03 02:15:18 +0300

    Add files via upload

--- js/f1/f1-analytics.js
+++ js/f1/f1-analytics.js
@@ -136,9 +136,10 @@ async function renderTeammateH2H() {
     var aPct = Math.round((a.pts / total) * 100);
     var col = (typeof TEAM_COLORS !== 'undefined' && TEAM_COLORS[tid]) || '#8a8fa8';
     html += '<div class="f1a-h2h">'
-      + '<div class="f1a-h2h-top"><span style="color:' + col + '">' + a.name + ' <strong>' + a.pts + '</strong></span>'
-      + teamLogo(tid, t.name)
-      + '<span style="color:' + col + '"><strong>' + b.pts + '</strong> ' + b.name + '</span></div>'
+      + '<div class="f1a-h2h-top">'
+      + '<span class="f1a-h2h-left" style="color:' + col + '">' + a.name + ' <strong>' + a.pts + '</strong></span>'
+      + '<span class="f1a-h2h-logo">' + teamLogo(tid, t.name) + '</span>'
+      + '<span class="f1a-h2h-right" style="color:' + col + '"><strong>' + b.pts + '</strong> ' + b.name + '</span></div>'
       + '<div class="f1a-h2h-bar"><div class="f1a-h2h-fill" style="width:' + aPct + '%;background:' + col + '"></div></div>'
       + '</div>';
   });
--- js/f1/f1-standings.js
+++ js/f1/f1-standings.js
@@ -54,7 +54,7 @@ function renderDriverStandings(drivers, round) {
       + '<span class="f1-pos ' + pc + '">' + pos + moveHtml + '</span>'
       + '<span class="f1-num" style="background:' + col + '22;color:' + col + '">' + num + '</span>'
       + '<div class="f1-driver-info">'
-      + '<span class="f1-driver-line"><span class="f1-driver-name">' + name + '</span>' + teamLogo(cid) + '</span>'
+      + '<span class="f1-driver-line">' + teamLogo(cid) + '<span class="f1-driver-name">' + name + '</span></span>'
       + '<div class="f1-con-bar"><div class="f1-con-fill" style="width:' + barW + '%;background:' + col + '"></div></div></div>'
       + '<span class="f1-wins">' + wins + '</span>'
       + '<span class="f1-pts">' + pts + '</span></div>';
