823abce80b8c04ec034b44ac848b1c10a3eb8bc0
moethiago <mo3ath306@gmail.com>
2026-09-10 09:54:46 +0300

    wain 2.0: sw cache bump

--- wain/sw.js
+++ wain/sw.js
@@ -1,4 +1,4 @@
-const C="wain-v1";
+const C="wain-v2";
 self.addEventListener("install",e=>{self.skipWaiting();});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
 self.addEventListener("fetch",e=>{
