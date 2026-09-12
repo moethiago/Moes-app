f164fe9a3ab41920ae6ea136a3abca555244e6b4
moethiago <mo3ath306@gmail.com>
2026-09-12 17:32:34 +0300

    bump sw cache to v14

--- maqadi/sw.js
+++ maqadi/sw.js
@@ -1,5 +1,5 @@
 // مقاضي — service worker: instant open, offline shell. API is never cached.
-var C = "maqadi-v13";
+var C = "maqadi-v14";
 var SHELL = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 self.addEventListener("install", function (e) {
   e.waitUntil(caches.open(C).then(function (c) { return c.addAll(SHELL); }).then(function () { return self.skipWaiting(); }));
